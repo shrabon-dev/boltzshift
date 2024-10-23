fa7f3db23ac6794aca9358b5c9cf179446fca7b7
Md. Shrabon Hossain <90843040+shrabon-dev@users.noreply.github.com>
2024-10-23 16:59:23 +0600

    header responsive done

--- src/components/Global.vue
+++ src/components/Global.vue
@@ -5,25 +5,25 @@
                 <p class="font-clash text-lg pt-3 text-third font-medium text-center">Proudly serving some of the top global companies</p>
             </div>
             <div class="content pt-20">
-                <div class="flex justify-evenly">
-                    <div class="w-1/6">
-                        <img class="w-1/2 mx-auto block" src="../assets/images/wavefun.png" alt="wavefun.png">
+                <div class=" flex flex-wrap md:flex-nowrap justify-evenly">
+                    <div class="w-1/2 pb-5 md:w-1/3 lg:w-1/6">
+                        <img class="md:w-1/2 mx-auto block" src="../assets/images/wavefun.png" alt="wavefun.png">
                     </div>
-                    <div class="w-1/6">
-                        <img class="w-1/2 mx-auto block" src="../assets/images/convertkit.png" alt="wavefun.png">
+                    <div class="w-1/2 pb-5 md:w-1/3 lg:w-1/6">
+                        <img class="md:w-1/2 mx-auto block" src="../assets/images/convertkit.png" alt="wavefun.png">
                     </div>
                 
-                    <div class="w-1/6">
-                        <img class="w-1/2 mx-auto block" src="../assets/images/doradesign.png" alt="wavefun.png">
+                    <div class="w-1/2 pb-5 md:w-1/3 lg:w-1/6">
+                        <img class="md:w-1/2 mx-auto block" src="../assets/images/doradesign.png" alt="wavefun.png">
                     </div>
-                    <div class="w-1/6">
-                        <img class="w-1/2 mx-auto block" src="../assets/images/Sisila.png" alt="wavefun.png">
+                    <div class="w-1/2 pb-5 md:w-1/3 lg:w-1/6">
+                        <img class="md:w-1/2 mx-auto block" src="../assets/images/Sisila.png" alt="wavefun.png">
                     </div>
-                    <div class="w-1/6">
-                        <img class="w-1/2 mx-auto block" src="../assets/images/proline.png" alt="wavefun.png">
+                    <div class="w-1/2 pb-5 md:w-1/3 lg:w-1/6">
+                        <img class="md:w-1/2 mx-auto block" src="../assets/images/proline.png" alt="wavefun.png">
                     </div>
-                    <div class="w-1/6">
-                        <img class="w-1/2 mx-auto block" src="../assets/images/doradesign.png" alt="wavefun.png">
+                    <div class="w-1/2 pb-5 md:w-1/3 lg:w-1/6">
+                        <img class="md:w-1/2 mx-auto block" src="../assets/images/doradesign.png" alt="wavefun.png">
                     </div>
                 </div>
             </div>
